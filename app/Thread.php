981e6735700f3dd264e213aade1e49c52252b511
Waleed H. Khan <info@waleed.de>
2017-12-20 21:30:41 +0100

    UnitTests & cleanup

--- app/Thread.php
+++ app/Thread.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+
+    public function path()
+    {
+        return '/threads/' . $this->id;
+    }
+
 }
--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -12,9 +12,26 @@ class ThreadsTest extends TestCase
 
     /** @test  */
 
-    public function a_user_can_browse_threads()
+    public function a_user_can_view_all_threads()
     {
+
+        $thread= factory('App\Thread')->create();
+
         $response = $this->get('/threads');
-        $response->assertStatus(200);
+        $response->assertSee($thread->title);
+
     }
+
+    /** @test  */
+
+    public function a_user_can_read_a_single_threads()
+    {
+
+        $thread= factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' .$thread->id);
+        $response->assertSee($thread->title);
+
+    }
+
 }
